6172ec69fb2fb3e0b669cb66c95d445a1a828197
aasibipin <aasibipin@gmail.com>
2019-07-21 16:42:01 -0400

    updates

--- WeatherApi.py
+++ WeatherApi.py
@@ -1,2 +1,12 @@
+import urllib.request
 import json
-import urllib3
+
+
+weather_api = "efd4253984a5d6d2f3edd53b754a12c0"
+url = "http://api.openweathermap.org/data/2.5/weather?q=Toronto,CA&units=metric&appid=%s"%weather_api 
+
+response = urllib.request.urlopen(url).read()
+
+json_obj = str(response, 'utf-8')
+data = json.loads(json_obj)
+
