5cb15caf98da7ac89e6fb80e422963b412c65b25
aasibipin <aasibipin@gmail.com>
2019-07-28 22:16:16 -0400

    reading ecg template

---
+++ ecganalysis.py
@@ -0,0 +1,18 @@
+import scipy
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+ecgdata = "bio_signals/t1/Saved_dataECG.csv"
+ecg = pd.read_csv(ecgdata, header=None, names= ["Time","IDK", "Value"])
+
+sample_size = 1000
+sample_ecg = ecg.iloc[30:sample_size,[0,2]]
+
+time = sample_ecg.iloc[:,0]
+value = sample_ecg.iloc[:,1]
+
+print (np.arange(time.size))
+print (value)
+plt.plot (np.arange(time.size),value)
+plt.show()
