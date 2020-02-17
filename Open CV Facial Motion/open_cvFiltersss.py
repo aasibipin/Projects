e8e5b3bf1ed9dc3b6297c96eadb3078bf65fc8c4
aasibipin <aasibipin@gmail.com>
2020-02-17 12:40:43 -0500

    updates

--- Open CV Facial Motion/open_cvFiltersss.py
+++ Open CV Facial Motion/open_cvFiltersss.py
@@ -6,25 +6,18 @@ while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    #hsv hue set value
-    lower_red = np.array([0,0,0])
-    upper_red = np.array([225,225,255])
+    lower_red = np.array([100,100,0])
+    upper_red = np.array([120,225,255])
     kernal = np.ones((10,10),np.uint8)
 
     mask = cv2.inRange(hsv, lower_red, upper_red)   #Mask = 1 or = 0 
-    opening = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernal)
-    closing = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernal)
     
     res = cv2.bitwise_and(frame, frame, mask=mask)
-    temp1 = cv2.bitwise_and(res, res, mask=opening)
-    final = cv2.bitwise_and(temp1, temp1, mask=closing)
 
-    median = cv2.medianBlur(final,15)
 
-    cv2.imshow('median',median)
-    cv2.imshow('opening',opening)
-    cv2.imshow('closing',closing)
-    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
