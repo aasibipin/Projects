890e3fce30870aa5f99200608cb4baed84e8c49c
aasibipin <aasibipin@gmail.com>
2020-02-27 12:44:11 -0500

    Determining Distance

--- Open CV Facial Motion/templatematchingvid.py
+++ Open CV Facial Motion/templatematchingvid.py
@@ -1,22 +1,29 @@
 import cv2
-import numpy as np 
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import style
+from statistics import mean
+
 cap = cv2.VideoCapture(0)
-template = cv2.imread('RSU.PNG')
+
+template = cv2.imread('ieee2.PNG')
 template_gray = cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
 
 w,h = template_gray.shape[::-1]
+locations = []
 
 while True:
+    cv2.waitKey(70)
     _, frame = cap.read()
     
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     res = cv2.matchTemplate(gray_frame, template_gray, cv2.TM_CCOEFF_NORMED)
-    thres = 0.5
-    loc = np.where(res>=thres)
-    
+    THRES = 0.4
+    loc = np.where(res>=THRES)
     for pt in zip(*loc[::-1]):
         cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0,255,0),2)
+        locations.append(pt)
 
     cv2.imshow("frame", frame)
 
@@ -29,6 +36,13 @@ returns a 32-bit integer corresponding to the pressed key
 & 0xFF is a bit mask which sets the left 24 bits to zero, because ord() 
  returns a value betwen 0 and 255, since your keyboard only has a limited character set
 '''
+
+plt.plot(*zip(*locations))
+plt.title('Displacement')
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.show()
+
 
 cv2.destroyAllWindows()
 cap.release()
