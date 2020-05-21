99612ffe776b40f5df5f0b68a82c6ea1504fc22a
aasibipin <aasibipin@gmail.com>
2020-05-20 23:31:56 -0400

    updates

--- TensorFlow/CNNs/CNN_MNIST.py
+++ TensorFlow/CNNs/CNN_MNIST.py
@@ -44,3 +44,35 @@ x_image = tf.reshape(x, shape= [-1, 28, 28, 1])
 # shape = [filter_height, filter_weight, in_channels, outchannels]
 W_conv1 = tf.Variable(tf.truncated_normal(shape=[5, 5, 1, 32], stddev=0.1))
 b_conv1 = tf.Variable(tf.constant(0.1, shape=[32])) # Need 32 biases for 32 outputs 
+
+convolve1 = tf.nn.conv2d(x_image, W_conv1, strides=[1,1,1,1], padding= 'SAME') + b_conv1
+h_conv1 = tf.nn.relu(convolve1)
+conv1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME') #max_pool_2x2
+
+W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
+b_conv2 = tf.Variable(tf.constant(0.1, shape=[64])) #need 64 biases for 64 outputs
+
+convolve2= tf.nn.conv2d(conv1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2
+h_conv2 = tf.nn.relu(convolve2)
+conv2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME') #max_pool_2x2
+
+layer2_matrix = tf.reshape(conv2, [-1, 7 * 7 * 64])
+
+W_fc1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1))
+b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024])) # need 1024 biases for 1024 outputs
+
+fcl = tf.matmul(layer2_matrix, W_fc1) + b_fc1
+h_fc1 = tf.nn.relu(fcl)
+
+keep_prob = tf.placeholder(tf.float32)
+layer_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+W_fc2 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1)) #1024 neurons
+b_fc2 = tf.Variable(tf.constant(0.1, shape=[10])) # 10 possibilities for digits [0,1,2,3,4,5,6,7,8,9]
+
+
+fc=tf.matmul(layer_drop, W_fc2) + b_fc2
+
+y_CNN= tf.nn.softmax(fc)
+y_CNN
