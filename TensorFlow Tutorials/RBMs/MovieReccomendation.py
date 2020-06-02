a016886256342850da2abdb689e1f0917b61fc49
aasibipin <aasibipin@gmail.com>
2020-06-02 17:08:48 -0400

    Recommendation Project

---
+++ TensorFlow Tutorials/RBMs/MovieReccomendation.py
@@ -0,0 +1,111 @@
+import tensorflow as tf 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pylab as plt 
+
+movies_df = pd.read_csv('ml-1m/movies.dat', sep= '::', header= None, 
+names=['MovieID', 'Title', 'Genres'], engine= 'python')
+ratings_df = pd.read_csv('ml-1m/ratings.dat', sep= '::', header=None, 
+names= ['UserID', 'MovieID', 'Rating', 'Timestamp'], engine='python')
+
+MAXRATE = 5.0
+# Number of movies = 3883
+
+# Normalized users ratings into a matrix of user-rating
+user_rating_df = ratings_df.pivot(index='UserID', columns='MovieID', values='Rating')
+ 
+norm_user_rating_df = user_rating_df.fillna(0) / MAXRATE
+trX = norm_user_rating_df.values
+
+# Setting Model Parameters
+# Hidden Units = number of features
+hiddenUnits = 20
+visibleUnits =  len(user_rating_df.columns)
+vb = tf.placeholder("float", [visibleUnits]) #Number of unique movies
+hb = tf.placeholder("float", [hiddenUnits]) #Number of features we're going to learn
+W = tf.placeholder("float", [visibleUnits, hiddenUnits])
+
+# Phase 1 Input Processing 
+v0 = tf.placeholder("float", [None, visibleUnits])          # Inital State of Visible
+_h0 = tf.nn.sigmoid(tf.matmul(v0, W) + hb)                  # Inital Probability of Hidden
+h0 = tf.nn.relu(tf.sign(_h0 - tf.random_uniform(tf.shape(_h0))))    # Inital State of Hidden
+# Phase 2: Reconstruction
+_v1 = tf.nn.sigmoid(tf.matmul(h0, tf.transpose(W)) + vb)    # Following Probability of Visible
+v1 = tf.nn.relu(tf.sign(_v1 - tf.random_uniform(tf.shape(_v1))))    # Following State of Visible 
+h1 = tf.nn.sigmoid(tf.matmul(v1, W) + hb)                   # Following Probability of Hidden
+
+# Set Training Parameters and Functions
+alpha = 1.0
+w_pos_grad = tf.matmul(tf.transpose(v0), h0)
+w_neg_grad = tf.matmul(tf.transpose(v1), h1)
+#Calculate the Contrastive Divergence to maximize
+CD = (w_pos_grad - w_neg_grad) / tf.to_float(tf.shape(v0)[0])
+#Create methods to update the weights and biases
+update_w = W + alpha * CD
+update_vb = vb + alpha * tf.reduce_mean(v0 - v1, 0)
+update_hb = hb + alpha * tf.reduce_mean(h0 - h1, 0)
+
+err = v0 - v1
+err_sum = tf.reduce_mean(err * err)
+
+#Current weight
+cur_w = np.zeros([visibleUnits, hiddenUnits], np.float32)
+#Current visible unit biases
+cur_vb = np.zeros([visibleUnits], np.float32)
+#Current hidden unit biases
+cur_hb = np.zeros([hiddenUnits], np.float32)
+#Previous weight
+prv_w = np.zeros([visibleUnits, hiddenUnits], np.float32)
+#Previous visible unit biases
+prv_vb = np.zeros([visibleUnits], np.float32)
+#Previous hidden unit biases
+prv_hb = np.zeros([hiddenUnits], np.float32)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+#  Train the RBM with 15 epochs with each epoch using 10 batches with size 100. 
+epochs = 15
+batchsize = 100
+errors = []
+for i in range(epochs):
+    for start, end in zip( range(0, len(trX), batchsize), range(batchsize, len(trX), batchsize)):
+        batch = trX[start:end]
+        cur_w = sess.run(update_w, feed_dict={v0: batch, W: prv_w, vb: prv_vb, hb: prv_hb})
+        cur_vb = sess.run(update_vb, feed_dict={v0: batch, W: prv_w, vb: prv_vb, hb: prv_hb})
+        cur_nb = sess.run(update_hb, feed_dict={v0: batch, W: prv_w, vb: prv_vb, hb: prv_hb})
+        prv_w = cur_w
+        prv_vb = cur_vb
+        prv_hb = cur_hb
+    errors.append(sess.run(err_sum, feed_dict={v0: trX, W: cur_w, vb: cur_vb, hb: cur_hb}))
+    print (errors[-1])
+plt.plot(errors)
+plt.ylabel('Error')
+plt.xlabel('Epoch')
+plt.show()
+
+# Select User and Recommend
+mock_user_id = 215
+
+#Selecting the input user
+inputUser = trX[mock_user_id-1].reshape(1, -1)
+inputUser[0:5]
+
+#Feeding in the user and reconstructing the input
+hh0 = tf.nn.sigmoid(tf.matmul(v0, W) + hb)
+vv1 = tf.nn.sigmoid(tf.matmul(hh0, tf.transpose(W)) + vb)
+feed = sess.run(hh0, feed_dict={ v0: inputUser, W: prv_w, hb: prv_hb})
+rec = sess.run(vv1, feed_dict={ hh0: feed, W: prv_w, vb: prv_vb})
+print(rec)
+
+# List the 20 most recommended movies for our mock user
+scored_movies_df_mock = movies_df[movies_df['MovieID'].isin(user_rating_df.columns)]
+scored_movies_df_mock = scored_movies_df_mock.assign(RecommendationScore = rec[0])
+scored_movies_df_mock.sort_values(["RecommendationScore"], ascending=False).head(20)
+
+# Find all the movies that our mock user has watched before
+movies_df_mock = ratings_df[ratings_df['UserID'] == mock_user_id]
+movies_df_mock.head()
+
+#Merging movies_df with ratings_df by MovieID
+merged_df_mock = scored_movies_df_mock.merge(movies_df_mock, on='MovieID', how='outer')
+merged_df_mock.sort_values(["RecommendationScore"], ascending=False).head(20)
--- TensorFlow Tutorials/RBMs/ReccomendationSystem.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-with urllib.request.urlopen("http://deeplearning.net/tutorial/code/utils.py") as url:
-    response = url.read()
-target = open('utils.py', 'w')
-target.write(response.decode('utf-8'))
-target.close()
-
