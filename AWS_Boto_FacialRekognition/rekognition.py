0e7463e325ccccf0714ff4918e24a660721b5beb
aasibipin <aasibipin@gmail.com>
2020-06-19 13:10:42 -0400

    Delete AWS

--- AWS_Boto_FacialRekognition/rekognition.py
+++ AWS_Boto_FacialRekognition/rekognition.py
@@ -2,9 +2,6 @@ import boto3
 import requests
 from pprint import pprint
 
-client = boto3.client('rekognition')
-
-
 def get_img_from_url(imgurl):
     resp = requests.get(imgurl)
     imgbytes = resp.content
@@ -14,13 +11,15 @@ def get_img_from_folder(filename):
     with open(filename, 'rb') as imgfile:
         return imgfile.read()
 
-img = get_img_from_url ('https://scontent.fykz1-1.fna.fbcdn.net/v/t1.15752-9/70487821_1041642329339731_5096054670380498944_n.jpg?_nc_cat=103&_nc_oc=AQlP1hzzarUbRsKdS9Yw4YMT5XdJqNbOsw4-1OKomHOUBl2gHG7poH3iIYMEvWQswnA&_nc_ht=scontent.fykz1-1.fna&oh=5aa7a2ef3848ac80b711748fecaf551e&oe=5E3D29C9')
 
-rekresp = client.detect_labels(Image ={'Bytes' : img}, MinConfidence = 30)             #Image is a map, therefore use {}
+url = 'https://www.extremetech.com/wp-content/uploads/2019/12/SONATA-hero-option1-764A5360-edit.jpg'
 
+client = boto3.client('rekognition')
+img = get_img_from_url (url)
+# Image is a map, therefore use {}
+rekresp = client.detect_labels(Image ={'Bytes' : img}, MinConfidence = 30)
 pprint(rekresp)
+
 
 for label in rekresp['Labels']:
     print (label ['Name'], label ['Confidence'])
-
-
